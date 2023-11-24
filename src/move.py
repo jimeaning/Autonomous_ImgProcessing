8e423c4d67a2a59c671c745e624cbe355e3981d6
exitdoor4 <exitdoor4@naver.com>
2023-11-24 00:18:10 -0500

    complete move.py

--- src/move.py
+++ src/move.py
@@ -1,11 +1,11 @@
 import rospy
 from geometry_msgs.msg import Twist
-import sys, select ,os
+import sys, select ,os, threading
 
 if os.name == 'nt':
     import msvcrt,time
 else:
-    import tty, termios
+    import tty, termios, time
     
 """"
 nt = windows NT 를 의미함
@@ -13,38 +13,101 @@ nt = windows NT 를 의미함
 윈도우 같은 경우에는 msvcrt가 담당한다.
 
 """
-def move():
-    rospy.init_node('autonomous_move')
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+
+
+def getKey():
     
-    twist = Twist()
-
-    print("Lets move your robot")
-    speed = 0.11
-    distance = 5
-    isForward = 1
-
-    if(isForward):
-        twist.linear.x = abs(speed)
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
     else:
-        twist.linear.x = -abs(speed)
+        key = ''
+    return key
+
+
+def stop_motion():
+    twist.linear.x= 0
+
+def startTimer():
+    global count, speed, green_flag, timer
+    count += 1
+    print("Count")
+    timer = threading.Timer(1, startTimer)
+    timer.start()
+    
+    
+    if green_flag == 1:
+        speed = 0.22
+        print("초록불임")
+        timer.cancel()
+    
+    if count == 5:
+        speed = 0
+        count =0
+        green_flag = 0
+        print("타이머를 멈춘다")
         
-    twist.linear.y = twist.linear.z = 0
-    twist.angular.x = twist.angular.y = twist.angular.z = 0
-    
-    
-
-    while not rospy.is_shutdown():
-        twist.linear.x = 0.22
-        pub.publish(twist)
-    if rospy.is_shutdown == 0:
-        print("rospy is shutdonw")
-
-
+        timer.cancel()
+        
+        
 if __name__ == '__main__':
     try:
         # Testing our function
-        move()
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+        rospy.init_node('autonomous_move')
+        
+        
+        twist = Twist()
+
+        msg = """
+        
+        Lets move your robot
+        
+        q = 신호등 발견
+        w = 빨간불 이다.
+        e = 초록불 이다.
+        z = 시작한다. 
+        m = 종료
+        """
+        print(msg)
+        twist.linear.x = twist.linear.y = twist.linear.z = 0
+        twist.angular.x = twist.angular.y = twist.angular.z = 0
+        
+        count = 0
+        speed = 0
+        current_speed = 0
+        green_flag = 0
+        pub.publish(twist)
+        
+        while not rospy.is_shutdown():
+            
+            key = getKey()
+            
+            if key == 'q':
+                print("신호등 발견")
+                speed= 0.05
+                green_flag = 0
+                
+                startTimer()
+                
+            elif key == 'e':
+                print("초록불이였다")
+                green_flag = 1
+                speed = 0.22
+
+            elif key == 'z':
+                print("시작하기")
+                speed = 0.22
+            elif key == 'm':
+                break
+            
+            if current_speed != speed:
+                print("속도" , speed)
+                current_speed = speed
+                twist.linear.x = speed
+                pub.publish(twist)
+    
     except rospy.ROSInterruptException: pass
     
     finally:
@@ -52,8 +115,4 @@ if __name__ == '__main__':
         twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
         twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
         pub.publish(twist)
-     
-    if os.name != 'nt':
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    
     
